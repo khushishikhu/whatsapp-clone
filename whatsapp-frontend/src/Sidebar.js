79eeb96610338ed42d81b2d1fe1cae55d1999d48
Khushi Gautam <65439761+khushishikhu@users.noreply.github.com>
2020-09-11 17:25:18 +0530

    frontend files

---
+++ whatsapp-frontend/src/Sidebar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Sidebar.css';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import {Avatar, IconButton } from '@material-ui/core';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchOutlined from '@material-ui/icons/SearchOutlined';
+import SidebarChat from './SidebarChat';
+
+function sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar_header">
+                <Avatar src='https://avatars2.githubusercontent.com/u/65439761?s=400&u=25c135329b09ecbdb3ddf74f269ac32123f25031&v=4'/>
+                <div className="sidebar_headerright">
+                    <IconButton>
+                    <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                     <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                    <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className='sidebar_search'>
+                <div className='sidebar_searchContainer'>
+                    <SearchOutlined/>
+                    <input type='text' placeholder='Search or Start new chat'/>
+                </div>
+            </div>
+
+            <div className='sidebar_chat'>
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
+
+            </div>
+        </div>
+    )
+}
+
+export default sidebar
---
+++ whatsapp-frontend/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBG2r5kQK3oJqpmHOoqPPAcHfxji3q2yWM",
+    authDomain: "whatsapp-mern-ceb4e.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-ceb4e.firebaseio.com",
+    projectId: "whatsapp-mern-ceb4e",
+    storageBucket: "whatsapp-mern-ceb4e.appspot.com",
+    messagingSenderId: "850957160570",
+    appId: "1:850957160570:web:cd5c1af5bfd8861df5ebc3"
+  };
